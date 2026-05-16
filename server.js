5da45be2cea0320ffc7b83163fa91ff023e836dd
vadlamaninandini08-cloud <vadlamaninandini08@gmail.com>
2026-05-16 14:11:36 +0530

    Refactor server.js for improved readability

--- server.js
+++ server.js
@@ -7,8 +7,6 @@ require('dotenv').config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static(_dirname));
-
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
