d2a5cbfbf38e40ac2ef15d0c2fc4dc46d3c5d201
vadlamaninandini08-cloud <vadlamaninandini08@gmail.com>
2026-05-16 14:33:47 +0530

    Refactor server.js to export app module
    Refactor server.js to export the app module instead of listening on a port directly. This allows for better testing and integration with other modules.

--- server.js
+++ server.js
@@ -44,11 +44,4 @@ app.post('/signin', async (req, res) => {
   }
 });
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+module.exports = app;
