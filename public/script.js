ea84dc64c38fdd627ab531a6186d487636c7d85c
vadlamaninandini08-cloud <vadlamaninandini08@gmail.com>
2026-05-17 15:00:25 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -8,7 +8,7 @@ document.getElementById('signupBtn')?.addEventListener('click', async (e) => {
     return;
   }
   
-  const res = await fetch('/signup', {
+  const res = await fetch('/api/signup', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ email, password })
